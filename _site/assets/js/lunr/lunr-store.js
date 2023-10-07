d60cdb0b62a9432c3ada9cbc1ec252d85e7961d7
kunsanglee <gosmdochee2@gmail.com>
2023-10-07 12:52:34 +0900

    clone test

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,181 @@
+var store = [{
+        "title": "[Error] Gitbush - LF will be replaced by CRLF in ...",
+        "excerpt":"📌 OS : Windows 10 Tool : Git Bash warning: LF will be replaced by CRLF in {file name}. The file will have its original line endings in your working directory. Github 업로드를 위해서, add를 하는 중 발견한 오류입니다. 자주 등장하는 건 아닌데 본 이후엔 파일 등록도 안되서 해결하고자 합니다....","categories": ["Error"],
+        "tags": ["gitbash","error"],
+        "url": "http://localhost:4000/error/GitBash-LF-whill-be-replaced/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] Jekyll - minimal mistakes 시작하기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Github는 Programming Code를 올리고 공개하며 확인할 수 있습니다. (사실 Github 자체에 코드를 자유롭게 올리고, REAMME를 쓰고, 익숙해지는 것도 꽤나 오래 걸렸죠😅) 각 Repositories마다 REAM.MD가 있어, 각각에 대한 설명을 적을 수 있습니다. 하지만 이론적인 부분과 오류사항 등에 대해 자세히 기재하기에는 어려웠습니다. 이는 Github의...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Start/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] minimal mistakes - config.yml 수정하기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Visual Studio Code 나의 블로그이다는 걸 보여주기 위해, 기본적인 정보를 넣고 정리하는게 중요합니다! 본인의 정보를 잘 정리해주시고, 본인의 정체성을 잘 들어내보세요! 저는 정리할 때, 뿌듯하고 또 제 노트를 칭찬해주던 고등학교, 학부 시절의 기억때문에, “정리가 재밌는 개발자”라는 정체성을 잡았습니다. 개발을 할때,...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-config/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] minimal mistakes - Category 세팅하기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Visual Studio Code 어느정도 세팅을 마쳤다면, 이제 상단의 Category를 준비해야합니다. 저는 현재의 제 약력과 전체적인 카테고리, 태그들을 모든 내용을 게시판으로 정리하였습니다. 이 외에도 연도별 등의 내용으로도 세팅하실 수 있습니다. 원하는 url만 잘 기재하시면, 표출하고싶으신 사이트(Github 등)로 연결하실 수도 있습니다. navigation.yml...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Category/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] minimal mistakes - 본문 영역 및 글자 크기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Visual Studio Code (minimal mistakes 제작자 Michael Rose Sample Post) minimal mistakes를 사용하기 전에 제일 머뭇거렸던 이유가 콘텐츠 영역의 너비가 너무 좁다는 것이였습니다. minimal mistakes 자체에서 wide page를 지원해줍니다. minimal mistakes의 공식 홈페이지 가이드 Wide-Page에서 직접 확인하실 수 있습니다. 하지만...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Content-Width/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] minimal mistakes - 파비콘(Favicon) 세팅하기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Visual Studio Code 파비콘(Favicon)이란, 인터넷 웹 브라우저의 주소창에 표시되는 대표 아이콘입니다. 기본적으로 세팅해두지 않으면, 크롬을 기준으로 이러한 지구본 모양만 보여지게 됩니다. 블로그의 정체성을 나타내기 위해선, 지구본보단 원하는 모양으로 바꾸는게 좋겠죠! 원하는 이미지 찾기 이미지는 직접 만드셔도 좋고, 찾으셔도 좋습니다. 저같은...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog","favicon"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Favicon/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] minimal mistakes - 포스팅 글 써보기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Visual Studio Code 이제 본격적으로 포스팅 올리는 방법에 대해서 설명드리겠습니다. minimal mistakes의 경우 양식이 정해져있어서, 한번 익숙해지시면 손쉽게 포스팅 가능하실껍니다. 폴더 생성하기 생성 파일 : github.io 폴더 &gt; _posts 폴더 이전에 docs 파일을 참고하여, 포스팅 글을 정리할 폴더 _posts를 생성하였습니다....","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Posting/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] minimal mistakes - 댓글 기능 추가하기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Visual Studio Code 블로그를 만드는 가장 중요한 건, 정리한 내용을 다른 이들과 공유하며, 모르는 분들은 도와주고, 틀린건 다시 배워가며 업데이트하는 맛이겠죠! 따라서 댓글 기능을 구현하는 건 매우 중요하다고 생각합니다. 저도 오류는 많았지만, 충분히 할 수 있었습니다! 걱정말고 진행해봅시다! DISQUS 가입하기...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog","disqus"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Comment/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] minimal mistakes - 방문자 통계(Analytics)하기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Visual Studio Code 블로그를 운영하는 가장 큰 힘이자 원동력이 아무래도 방문자겠죠. 그래서 얼마만큼의 방문자가 접속했는지가 가장 궁금하게 됩니다. 따라서 이를 분석해서 제공해주는 google Analytics를 적용해보고자 합니다. Google Analytics 등록하기 1) Google Analytics 에 접속하시면 무료로 시작할 수 있습니다. 당연히도 Google...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog","google analytics"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Analytics/",
+        "teaser": null
+      },{
+        "title": "한국전자통신연구소(ETRI) 하계인턴 후기 - 2019년 하계",
+        "excerpt":"💡 2019년 한국전자통신연구소(ETRI) 하계 인턴 후기입니다. 2019년 7월부터 8월까지 이수했으며, 현재 전형과 다른 내용이 많지만 궁금하신 분들께 도움이 되고자 적어봅니다. 정보보단 후기이자 회고록처럼 작성하기에 가볍게 읽어주세요. 지원 전 2019년, 학부생 4학년이자 학생회장을 맡으면서 정신없이 지내고 있었습니다. 무엇보다 졸업이 다가오며 좋은 학점 외의 개발 경험과 스킬 없는 스스로에게 크게 딜레마가 왔었습니다....","categories": ["Retrospective"],
+        "tags": ["etri","intern","retrospective"],
+        "url": "http://localhost:4000/retrospective/ETRI_Intern/",
+        "teaser": null
+      },{
+        "title": "컴퓨터공학부 16학번 졸업 회고록 - 2020",
+        "excerpt":"💡 졸업생의 학부생활 회고록입니다. 16학번의 휴학없는 2016년 3월 ~ 2020년 2월 칼졸업 회고록입니다. 대학 입학 전 프로그래밍은 경험해보지 않은 지방 4년제 대학의 졸업자의 회고록입니다 :) 4년간의 기록이라 글이 많이 길게 작성되어있으며, (화석이라) 실제 학교의 커리큘럼과는 차이가 있을 수 있습니다. 입학 전 사실 고등학교 1학년까지 피아노부터 클래식 기타(음악 실기)를 준비하면서, 청음도...","categories": ["Retrospective"],
+        "tags": ["retrospective"],
+        "url": "http://localhost:4000/retrospective/INJE-University/",
+        "teaser": null
+      },{
+        "title": "온라인 프로그래밍 교육 1인 사업 회고록 - 2020년",
+        "excerpt":"💡 온라인 프로그래밍 교육 1인 사업 회고록입니다. 4년제 대학을 휴학없이 칼 졸업한 후 해본 경험입니다. 요즘 각광받는 온라인 교육, 스스로 운영하고 마무리한 회고록입니다. :) 창업 이유 2020년 2월, 드디어 4년제 대학을 졸업하게 되었습니다. 졸업하고 취업준비를 하는 것이 당연한 수순같았지만, 졸업하는 기점으로의 제 실력은 기업원서를 써볼만큼의 자신감이 없습니다. 😥 무엇보다 확실하게...","categories": ["Retrospective"],
+        "tags": [],
+        "url": "http://localhost:4000/retrospective/OneToOneProgramming/",
+        "teaser": null
+      },{
+        "title": "개발자 1년 회고록 - 2020",
+        "excerpt":"💡 2020년, 개발자로 어떻게 보냈는지 돌아보는 회고록입니다. 학생이 아니였던 첫 해였으며, 제 인생에 다양한 방향으로 진행해보았습니다. 2020년을 어떻게 보냈는지 확인해보고자하니 잘한 것은 응원으로, 잘못한건 격려로 부탁드립니다. 나의 2020년 Keyword 키워드 컬러는 펜톤 디자인에서 발표한 2020년의 색, ‘클래식 블루’를 기준으로 세팅하였습니다. 2020년 2월이 되어서야 학생의 신분을 버리게 되었습니다. 낯선 곳으로 입학한게...","categories": ["Retrospective"],
+        "tags": [],
+        "url": "http://localhost:4000/retrospective/2020_year/",
+        "teaser": null
+      },{
+        "title": "SSAFY 5기 후기 - 삼성SW청년아카데미 합격",
+        "excerpt":"💡 삼성SW청년아카데미 SSAFY 5기 후기입니다. 교육준비, 합격후기, 교육과정 후기 등 SSAFY 과정을 마칠 때까지 시시때때로 업데이트 될 예정입니다. 보안상 공개할 수 없는 내용은 생략하였으니, 감안하고 읽어주시길 바랍니다. ❔ SSAFY란? 현재 공개 자료는 SSAFY 공식 페이지에서 공개되는 정보 외에는 보안상의 이유로 제공하지 않습니다. 지원 시 개인적으로 참고했던 점을 정리한 내용입니다. 더욱...","categories": ["Retrospective"],
+        "tags": [],
+        "url": "http://localhost:4000/retrospective/SSAFY/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] minimal mistakes - 검색창 노출시키기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Google Search Console, Visual Studio Code 블로그를 꾸며간다는 것은, 더 많은 사람들이 내 글을 읽어줬으면 좋겠죠! 그렇게 되려면, 키워드를 검색했을 때 보이는 것이 제일 중요할 겁니다.😎 블로그의 첫 시리즈이자 현재 블로그의 세팅과 관련되 minimal mistakes의 마지막 포스트! Google search console...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog","google search console"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Search/",
+        "teaser": null
+      },{
+        "title": "[A to Z] Github Blog Jekyll minimal mistakes",
+        "excerpt":"💡 A to Z는 개별적으로 작성한 게시글을 한 번에 볼 수 있게 정리하였습니다. 사용의 팁이나 고려하면 좋은 사항 등의 부가설명은 생략되고, 핵심 내용만 넣었습니다. 상세한 설명과 관련 이야기(시행착오 및 전처리 작업 등)는 개별 포스트에 있습니다. [00] Github Blog A to Z 기본 세팅 : Ruby, Jekyll, minimal-mistakes 테마 적용, github...","categories": ["A_to_Z"],
+        "tags": ["minimal mistakes","jekyll","blog","favicon","disqus","google analytics"],
+        "url": "http://localhost:4000/a_to_z/GithubBlog/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 1000번 A+B",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A+B를...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/1000/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 1001번 A-B",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A-B를...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/1001/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 1008번 A/B",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오. 입력 첫째 줄에 A와 B가 주어진다. (0 &lt; A, B &lt; 10) 출력 첫째 줄에 A/B를...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/1008/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 1110번 더하기 사이클",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다. 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/1110/",
+        "teaser": null
+      },{
+        "title": "[A to Z] Beakjoon Algorithms Solution",
+        "excerpt":"💡 A to Z는 개별적으로 작성한 게시글을 한 번에 볼 수 있게 정리하였습니다. 문제의 상세한 풀이가 아닌 작성자가 푼 문제를 한눈에 볼 수 있도록 정리하였습니다. 상세한 설명과 관련 이야기(시행착오 및 로직 설명 등)는 개별 포스트에 있습니다. Beakjoon 시작?! 어느 정도 개발을 했다고 하면 자연스럽게 코딩테스트를 준비하거나 연습하기 위해 들어온다는 Beakjoon...","categories": ["A_to_Z"],
+        "tags": ["beakjoon","algorithm","solution"],
+        "url": "http://localhost:4000/a_to_z/BeakjoonSolution/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 2475번 검증수",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 컴퓨터를 제조하는 회사인 KOI 전자에서는 제조하는 컴퓨터마다 6자리의 고유번호를 매긴다. 고유번호의 처음 5자리에는 00000부터 99999까지의 수 중 하나가 주어지며 6번째 자리에는 검증수가 들어간다. 검증수는 고유번호의 처음...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/2475/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 1158번 요세푸스 문제",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 요세푸스 문제는 다음과 같다. 1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다. 이제 순서대로 K번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/1158/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 1193번 분수찾기",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 무한히 큰 배열에 다음과 같이 분수들이 적혀있다. 1/1 1/2 1/3 1/4 1/5  … 2/1 2/2 2/3 2/4  …  … 3/1 3/2 3/3  …  …  … 4/1 4/2  …  …  …  … 5/1 …  …  …  …  … …  …  …  …  …  … 이와 같이 나열된 분수들을 1/1 -&gt; 1/2 -&gt; 2/1 -&gt; 3/1 -&gt; 2/2...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/1193/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 1074번 Z",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 한수는 크기가 2N × 2N인 2차원 배열을 Z모양으로 탐색하려고 한다. 예를 들어, 2×2배열을 왼쪽 위칸, 오른쪽 위칸, 왼쪽 아래칸, 오른쪽 아래칸 순서대로 방문하면 Z모양이다. 만약, N...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/1074/",
+        "teaser": null
+      },{
+        "title": "[A to Z] SW Expert Academy Algorithms Solution",
+        "excerpt":"💡 A to Z는 개별적으로 작성한 게시글을 한 번에 볼 수 있게 정리하였습니다. 문제의 상세한 풀이가 아닌 작성자가 푼 문제를 한눈에 볼 수 있도록 정리하였습니다. 상세한 설명과 관련 이야기(시행착오 및 로직 설명 등)는 개별 포스트에 있습니다. SWEA?! 삼성에서 운영하고, 알고리즘을 비롯해 다양한 학습도 할 수 있는 사이트입니다. 사실 백준, 프로그래머스...","categories": ["A_to_Z"],
+        "tags": ["SWEA","algorithm","solution"],
+        "url": "http://localhost:4000/a_to_z/SWEASolution/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 2161번 카드1",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다. 이제 다음과...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/2161/",
+        "teaser": null
+      },{
+        "title": "[Github Blog] MarkDown 프로그래밍 코드 작성하기",
+        "excerpt":"📌 작성자 개발 환경 OS : Windows 10 Tool : Github Gist 기술 블로그를 작성하면, 프로그래밍 코드가 아예 없는 글을 작성하긴 힘듭니다! 알고리즘 코드를 작성하거나, 기본 개념부터 개발자는 프로그래밍 코드와 뗄 수 없는 사이죠. 프로그래밍 코드 블록을 작성하는 다양한 방법을 소개해드리겠습니다. 저 역시 많이 고민했는데, 알게 되어서 이렇게 공유해봅니다! MD...","categories": ["Github_Blog"],
+        "tags": ["minimal mistakes","jekyll","blog"],
+        "url": "http://localhost:4000/github_blog/GithubBlog-Code/",
+        "teaser": null
+      },{
+        "title": "[Beakjoon] 16956번 늑대와 양",
+        "excerpt":"📣 Beakjoon에서 PASS된 코드만 업데이트합니다. 알고리즘을 먼저 풀이하는 언어(Java)가 정해져있어, 풀이 언어(Python, C++, Java)가 모두 업데이트될 때까지는 시간이 걸릴 수 있습니다. 문제 제시 문제 크기가 R×C인 목장이 있고, 목장은 1×1 크기의 칸으로 나누어져 있다. 각각의 칸에는 비어있거나, 양 또는 늑대가 있다. 양은 이동하지 않고 위치를 지키고 있고, 늑대는 인접한 칸을...","categories": ["Beakjoon"],
+        "tags": ["algorithm","beakjoon"],
+        "url": "http://localhost:4000/beakjoon/16956/",
+        "teaser": null
+      },{
+        "title": "🕊블로그 이사했습니다.🕊",
+        "excerpt":"Github io를 해보고싶어서 2020년 7월 1일에 첫 커밋을 남기면서 시작했네요. jekyll 공부 및 minimal mistakes 공부 : 2020.07.01 ~ 2020.10.00 쯤? 커스터마이징 및 포스팅 : 2020.10.17 ~ 2021.07.08 약 1년의 기간 동안 정말 다양한 걸 해봤습니다. minimal mistakes도 공식홈페이지에서 설명이 워낙 잘되어있습니다. 비록 영문이지만… 순서대로 간 걸 생각하면, minimal mistakes...","categories": ["공지"],
+        "tags": [],
+        "url": "http://localhost:4000/%EA%B3%B5%EC%A7%80/GoodBye/",
+        "teaser": null
+      }]
